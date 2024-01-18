51799861328627daa98eba81cc29c8949a97498e
Azis kurniawan <aziskurniawan200@gmail.com>
2024-01-18 15:10:56 +0700

    menambhakan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident maxime quasi vero necessitatibus impedit aut!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
